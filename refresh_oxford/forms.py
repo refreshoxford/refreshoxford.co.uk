1e0d2cd2b803c4d2650639e2d1449eec413f660c
Charlie Denton <charlie@meshy.co.uk>
2012-07-19 16:37:23 +0100

    Make sign-up form nice and crispy.

--- refresh_oxford/forms.py
+++ refresh_oxford/forms.py
@@ -1,4 +1,7 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Attendee
 
@@ -7,3 +10,14 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = Attendee
 
+    helper = FormHelper()
+    helper.form_method = 'post'
+    helper.layout = Layout(
+        Fieldset('',
+            'name',
+            'email',
+            'github_username',
+            'extra',
+            Submit('submit', _('Sign Up'), css_class='submit'),
+        )
+    )
--- refresh_oxford/models.py
+++ refresh_oxford/models.py
@@ -4,9 +4,8 @@ from django.db import models
 class Attendee(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
-    github_username = models.CharField(max_length=255, null=True, blank=True)
-    extra = models.TextField(null=True, blank=True)
+    github_username = models.CharField(max_length=255, null=True, blank=True, help_text='(Optional)')
+    extra = models.TextField(null=True, blank=True, help_text='(Dietary requirements, etc...)')
 
     def __unicode__(self):
         return self.name
-
