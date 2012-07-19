900f0898abce34986c583744e4f760228fd64d8c
George Hickman <george@ghickman.co.uk>
2012-07-19 21:07:49 +0100

    add messages

--- refresh_oxford/views.py
+++ refresh_oxford/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import CreateView
 
 from .forms import SignUpForm
@@ -9,4 +10,9 @@ class SignUp(CreateView):
     model = Attendee
     success_url = '/'
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        msg = "Thanks for signing up, we'll email you more details closer to the date."
+        messages.info(self.request, msg)
+        return super(SignUp, self).form_valid(form)
 
